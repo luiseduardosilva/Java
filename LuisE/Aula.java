322e18c7045e9d90e84b81121908dc1630b3bb8e
Luís Eduardo <luissilva7991@gmail.com>
2017-02-16 20:07:59 -0300

    Create Aula.java

---
+++ LuisE/Aula.java
@@ -0,0 +1,39 @@
+package luisE;
+
+public class Aula {
+	
+	private String nomeObj;
+	private String corObj;
+	private String materialObj;
+	
+	public String getNomeObj()
+	{
+		return nomeObj;
+	}
+	
+	public void setNomeObj(String nomeObj)
+	{
+		this.nomeObj = nomeObj;
+	}
+	
+	public String getCorObj()
+	{
+		return corObj;
+	}
+	
+	public void getCorObj(String corObj)
+	{
+		this.corObj = corObj;
+	}
+	
+	public String setMaterialObj()
+	{
+		return materialObj;
+	}
+	
+	public void setmaterialObj(String materialObj)
+	{
+		this.materialObj = materialObj;
+	}
+	
+}
