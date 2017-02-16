18faac5b45f6235ead1c105ae6197e0c218c3afa
Luís Eduardo <luissilva7991@gmail.com>
2017-02-16 20:08:15 -0300

    Create Prin.java

---
+++ LuisE/Prin.java
@@ -0,0 +1,10 @@
+package luisE;
+
+public class Prin {
+	public static void main(String[] args){
+		Aula objeto = new Aula();
+		objeto.setNomeObj("Luis");
+		
+		System.out.println(objeto.getNomeObj());
+	}
+}
