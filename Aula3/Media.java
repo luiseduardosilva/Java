cbad928a480ddded20b744bcbee8e93ffc965d76
Luís Eduardo <luissilva7991@gmail.com>
2017-02-17 00:23:22 -0300

    Update Media.java
    Aprimoramento de código.

--- Aula3/Media.java
+++ Aula3/Media.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Media {
 	public static void main(String[] args){
-		float valor, valor2, valor3, soma;
+		float valor, valor2, valor3, media;
+		
 		Scanner scan = new Scanner(System.in);
+		
+		System.out.format("Informe a 1º nota: ");
 		valor = scan.nextFloat();
+		
+		System.out.format("Informe a 2º nota: ");
 		valor2 = scan.nextFloat();
+		
+		System.out.format("Informe a 3º nota: ");
 		valor3 = scan.nextFloat();
-		soma = (valor + valor2 + valor3) / 3;
-		System.out.println(soma);
-		if (soma >= 7){
+		
+		media = (valor + valor2 + valor3) / 3;
+		System.out.println(media);
+		if (media >= 7){
 			System.out.println("Aprovado");
 		}
-		else if (soma >= 5 && soma <= 7){
+		else if (media >= 5 && media <= 7){
 			System.out.println("Final");
 		} else{
 			System.out.println("Reprovado!");
