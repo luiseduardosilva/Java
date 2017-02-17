e4b55db98d39c35c08e85e1e2787d61d01f26057
Luís Eduardo <luissilva7991@gmail.com>
2017-02-17 00:23:53 -0300

    Update Consumo.java

--- Aula3/Consumo.java
+++ Aula3/Consumo.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Consumo {
 	public static void main(String[] args){
-		float combustivel;
-		float distancia;
-		float total;
+		float	combustivel;
+		float	distancia;
+		float	total;
+		
 		Scanner scan = new Scanner(System.in);
+		
 		System.out.format("Combustivel gasto: ");
 		combustivel = scan.nextFloat();
+		
 		System.out.format("Distancia percorrida: ");
 		distancia = scan.nextFloat();
+		
 		total = distancia / combustivel;
 		System.out.println(total);
 	}
