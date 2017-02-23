681d31f55de24a1221b280706d390d78874a4875
Luís Eduardo <luissilva7991@gmail.com>
2017-02-22 23:24:07 -0300

    Create Salario.java

---
+++ lista2/Questao1/Salario.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Salario {
+	private double salario;
+	private double aumento;
+	
+	public int getSalario()
+	{
+		return salario;
+	}
+	
+	public void setSalario(double salario)
+	{
+		this.salario = salario;
+	}
+	
+	public int getAumeto()
+	{
+		return aumento;
+	}
+	
+	public void setAumento(double aumento)
+	{
+		this.salario = aumento;
+	}
+}
