193b2de14be5b9b1f0dd56ff6d9deb4bc62c85c4
Luís Eduardo <luissilva7991@gmail.com>
2017-02-23 21:48:34 -0300

    Create ContaCorrente.java

---
+++ 23.02.2017/ContaCorrente.java
@@ -0,0 +1,41 @@
+
+public class ContaCorrente {
+	
+	private int numeroConta, agencia;
+	private float saldo;
+	private String cliente;
+	
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public float getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	public String getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+}
