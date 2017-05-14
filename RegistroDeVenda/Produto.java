cf89954eaa0e3f81b4471280fd37c3edb0ba3c75
Luís Eduardo <luissilva7991@gmail.com>
2017-05-14 17:22:41 -0300

    Create Produto.java

---
+++ RegistroDeVenda/Produto.java
@@ -0,0 +1,60 @@
+package br.luis;
+
+public class Produto {
+	
+	private String codigo, descricao;
+	private float valorVenda, valorCusto;
+	private boolean promocao;
+	
+	
+	public Produto(String codigo, String descricao, float valorVenda, float valorCusto, boolean promocao){
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.valorVenda = valorVenda;
+		this.valorCusto = valorCusto;
+		this.promocao = promocao;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(float valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public float getValorCusto() {
+		return valorCusto;
+	}
+
+	public void setValorCusto(float valorCusto) {
+		this.valorCusto = valorCusto;
+	}
+
+	public boolean isPromocao() {
+		return promocao;
+	}
+
+	public void setPromocao(boolean promocao) {
+		this.promocao = promocao;
+	}
+	
+	
+	
+}
