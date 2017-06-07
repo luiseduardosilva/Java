a21950cf1a66947cbf4704c3059bd217c6c391ed
Luís Eduardo <luissilva7991@gmail.com>
2017-06-06 23:16:33 -0300

    Create Vendedor.java

---
+++ RegistroDeVenda2/Vendedor.java
@@ -0,0 +1,51 @@
+package br.luis;
+
+import java.io.Serializable;
+
+public class Vendedor implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String nome, codigo, endereco;
+	private float comissao;
+	
+	public Vendedor(String nome, String endereco, String codigo){
+		this.nome 		= nome;
+		this.endereco	= endereco;
+		this.codigo 	= codigo;
+	}
+	
+	// Metodos
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public float getComissao() {
+		return comissao;
+	}
+	
+	public void setComissao(float comissao) {
+		this.comissao = comissao;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+		
+}
