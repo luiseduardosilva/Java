4143d83f9347d50ebd7cd10207056d08e88581f3
Luís Eduardo <luissilva7991@gmail.com>
2017-06-06 23:16:51 -0300

    Create Menu.java

---
+++ RegistroDeVenda2/Menu.java
@@ -0,0 +1,9 @@
+package br.luis;
+
+public class Menu {
+	public void exibeMenu(){
+		System.out.println("[1] Produto");
+		System.out.println("[2] Procurar");
+		System.out.println("[0] Exit");
+	}
+}
